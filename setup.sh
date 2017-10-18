930a047a9852522c3517a0b4c9c20d4b8ad74e84
Gareth Brown <gareth@gbmail.biz>
2017-10-18 15:19:38 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# after cloning the repository, set directory structure and any necessary basic variables
+
+# ask for project name
+
+# set project name var, set roles_path in ansible.cfg and check/set basedir
+
+# ask for basic vars: company name, etc.
+
+# set basic vars
+
+# pull common role submodules
+
+# create any necessary directory structure
+
+# verify success
