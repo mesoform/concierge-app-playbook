88a8ca511d8af6cfaa01389204dd7a68d99bfc3b
Gareth Brown <gareth@gbmail.biz>
2017-10-19 23:54:57 +0100

    add git init and set roles_path in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 
-# after cloning the repository, set any necessary basic variables
-ANSIBLE_HOME=$(echo ${ANSIBLE_HOME})
+# Initialise Git
+echo -e "Initialising Git"
+rm -Rf .git
+git init
+
+
+# after initialising git, set any necessary basic variables
+echo -e "checking existence of \${ANSIBLE_HOME}"
 if [ -z ${ANSIBLE_HOME} ]; then
-  ANSIBLE_HOME=/etc/ansible
-  echo -e "No \${ANSIBLE_HOME} found. Set \${ANSIBLE_HOME} ($ANSIBLE_HOME):"
+  DEFAULT_ANSIBLE_HOME=/etc/ansible
+  echo -e "No \${ANSIBLE_HOME} found.\nSet \${ANSIBLE_HOME} (${DEFAULT_ANSIBLE_HOME}):"
   read -p ">" ANSIBLE_HOME
+else
+  echo -e "\${ANSIBLE_HOME} set to ${ANSIBLE_HOME}"
 fi
 
 # ask for project name
-echo "what is the name of your application?"
-read APP_NAME
+# echo "what is the name of your application?"
+# read APP_NAME
 # set project name var, set roles_path in ansible.cfg and check/set basedir
 #sed -i "s/  project_name: SETUP_ENV/  project_name: ${APP_NAME}/" vars/main.yml
 
@@ -22,16 +30,13 @@ while read -r line; do
   KEY=$(echo ${var} | awk -F: '{sub(/^[ \t]+/, ""); print $1}')
   echo "set ${KEY} (${DESC}):"
   read -p ">" VAL
-  [ ${KEY} == "project_name" ] && PROJECT=${VAL}
-  sed "s/  ${KEY}: SETUP_ENV.*/  ${KEY}: ${VAL} #${DESC}/" vars/main.yml
+  [ ${KEY} == "project_name" ] && CONCIERGE_PROJECT=${VAL}
+  sed "s|  ${KEY}: SETUP_ENV.*|  ${KEY}: ${VAL} #${DESC}|" vars/main.yml
 done
 
-# pull common role submodules
-
-# create any necessary directory structure
 
 # set ansible roles_path
-# sed -i
+sed "s|roles_path = /etc/ansible/roles/CONCIERGE_PROJECT|roles_path = /etc/ansible/roles/${CONCIERGE_PROJECT}|" ansible.cfg
 
 # verify success
 
