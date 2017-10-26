9ff65acb3ff51eaefd5cfa2f8e2e48a840e36d88
Gareth Brown <gareth@gbmail.biz>
2017-10-26 20:04:01 +0100

    setup.sh: add more conditions and project repo URL

--- setup.sh
+++ setup.sh
@@ -20,6 +20,13 @@ if [ "${PARAM}" == "--initialise-git" ]; then
     git submodule add https://github.com/mesoform/create-concierge-app.git
     git submodule add https://github.com/mesoform/create-concierge-image.git
     git submodule add https://github.com/mesoform/create-concierge-tests.git
+
+    echo "set remote repository location"
+    read -e -p "Enter project repository URL: " PROJECT_REPO
+    echo "setting project repo"
+    git remote add origin ${PROJECT_REPO}
+    echo "set project repo:"
+    git remote -v
 fi
 
 
@@ -27,8 +34,8 @@ fi
 echo -e "checking existence of \${ANSIBLE_HOME}"
 if [ -z ${ANSIBLE_HOME} ]; then
     DEFAULT_ANSIBLE_HOME=/etc/ansible
-    echo -e "No \${ANSIBLE_HOME} found.\nSet \${ANSIBLE_HOME} (${DEFAULT_ANSIBLE_HOME}):"
-    read -e -p ">" ANSIBLE_HOME
+    echo -e "No \${ANSIBLE_HOME} found."
+    read -e -p "Enter \${ANSIBLE_HOME} (${DEFAULT_ANSIBLE_HOME}): " ANSIBLE_HOME
     ANSIBLE_HOME="${ANSIBLE_HOME:-${DEFAULT_ANSIBLE_HOME}}"
 else
     echo -e "\${ANSIBLE_HOME} set to ${ANSIBLE_HOME}"
@@ -36,22 +43,28 @@ fi
 
 while read -r -u9 line; do
     (
+        DEFAULT_PROJECT_NAME=$(basename $(echo ${PWD}))
+        DEFAULT_CONSUL_AS_AGENT=true
         DESC=$(echo ${line} | awk -F\# '{sub(/\#[ \t]+/, "#"); print $2}')
         KEY=$(echo ${line} | awk -F: '{sub(/^[ \t]+/, ""); print $1}')
-        echo "set ${KEY} (${DESC}):"
-        read -p ">" VAL
-        while [[ -z ${VAL} ]]; do
-            echo -e "value can't be empty"
-            echo "set ${KEY} (${DESC}):"
-            read -p ">" VAL
-        done
         if [[ ${KEY} == "project_name" ]]; then
-            # set ansible roles_path
+            read -e -p "Enter value for ${KEY} (${DESC} DEFAULT=${DEFAULT_PROJECT_NAME}): " VAL
+            VAL="${VAL:-${DEFAULT_PROJECT_NAME}}"
+            # set Ansible roles_path
             echo "appending local directory to roles_path"
             sed -i .swp -e "s|roles_path = CONCIERGE_PROJECT|roles_path = ${ANSIBLE_HOME}/roles/${VAL}|" ./ansible.cfg
+        elif [[ ${KEY} == "consul_as_agent" ]]; then
+            read -e -p "Enter value for ${KEY} (${DESC} DEFAULT=${DEFAULT_CONSUL_AS_AGENT}): " VAL
+            VAL="${VAL:-${DEFAULT_CONSUL_AS_AGENT}}"
+        else
+            read -p "Enter value for ${KEY} (${DESC}): " VAL
+            while [[ -z ${VAL} ]]; do
+                echo -e "value can't be empty"
+                read -p "Enter value for ${KEY} (${DESC}): " VAL
+            done
         fi
         echo "set ${KEY} = ${VAL}"
-        sed -i .swp -e "s|  ${KEY}: SETUP_ENV.*|  ${KEY}: ${VAL} #${DESC}|" vars/main.yml
+        sed -i .swp -e "s|  ${KEY}: SETUP_ENV.*|  ${KEY}: ${VAL} # ${DESC}|" vars/main.yml
    )
 done 9< <(grep SETUP_ENV vars/main.yml)
 
@@ -67,5 +80,4 @@ echo -e "tidying up..."
 [[ -e vars/main.yml.swp ]] && rm vars/main.yml.swp
 
 # verify success
-
 
