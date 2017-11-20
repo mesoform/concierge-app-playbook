c8af3febe0bb4fe7a91166d681447bc456b17d57
Gareth Brown <gareth@gbmail.biz>
2017-11-20 18:10:52 +0000

    setup.sh: better formatting & success condition

--- setup.sh
+++ setup.sh
@@ -7,10 +7,11 @@ DEFAULTS_FILE=defaults/main.yml
 if [ "${PARAM}" == "--initialise-git" ]; then
     GIT_INITIALISATION=TRUE
     # Initialise Git
-    echo -e "Initialising Git"
+    echo -e "Initialising Git\n"
     rm -Rf .git
     git init
-    echo "Adding submodules"
+    echo -e "\n\n"
+    echo -e "Adding submodules"
     for dir in $(ls -d create-concierge-* ); do
         if [ "$(ls -A ${dir} )" ]; then
             echo "${dir} submodule already setup"
@@ -19,21 +20,25 @@ if [ "${PARAM}" == "--initialise-git" ]; then
             rmdir ${dir}
         fi
     done
+    echo -e "\n\n"
     echo "Syncing submodules"
     git submodule add https://github.com/mesoform/create-concierge-app.git
     git submodule add https://github.com/mesoform/create-concierge-image.git
     git submodule add https://github.com/mesoform/create-concierge-tests.git
 
+    echo -e "\n\n"
     echo "set remote repository location"
     read -e -p "Enter project repository URL: " PROJECT_REPO
     echo "setting project repo"
     git remote add origin ${PROJECT_REPO}
+    echo -e "\n\n"
     echo "set project repo:"
     git remote -v
 fi
 
 
 # after initialising git, set any necessary basic variables
+echo -e "Let's set up some defaults\n\n"
 echo -e "checking existence of \${ANSIBLE_HOME}"
 if [ -z ${ANSIBLE_HOME} ]; then
     DEFAULT_ANSIBLE_HOME=/etc/ansible
@@ -41,7 +46,7 @@ if [ -z ${ANSIBLE_HOME} ]; then
     read -e -p "Enter \${ANSIBLE_HOME} (${DEFAULT_ANSIBLE_HOME}): " ANSIBLE_HOME
     ANSIBLE_HOME="${ANSIBLE_HOME:-${DEFAULT_ANSIBLE_HOME}}"
 else
-    echo -e "\${ANSIBLE_HOME} set to ${ANSIBLE_HOME}"
+    echo -e "\${ANSIBLE_HOME} set to ${ANSIBLE_HOME}\n\n"
 fi
 
 while read -r -u9 line; do
@@ -54,16 +59,18 @@ while read -r -u9 line; do
             read -e -p "Enter value for ${KEY} (${DESC} DEFAULT=${DEFAULT_PROJECT_NAME}): " VAL
             VAL="${VAL:-${DEFAULT_PROJECT_NAME}}"
             # set Ansible roles_path
-            echo "appending local directory to roles_path"
+            echo -e "appending local directory to roles_path\n\n"
             sed -i .swp -e "s|roles_path = CONCIERGE_PROJECT|roles_path = ${ANSIBLE_HOME}/roles/${VAL}|" ${CONFIG_FILE}
         elif [[ ${KEY} == "consul_as_agent" ]]; then
             read -e -p "Enter value for ${KEY} (${DESC} DEFAULT=${DEFAULT_CONSUL_AS_AGENT}): " VAL
             VAL="${VAL:-${DEFAULT_CONSUL_AS_AGENT}}"
+            echo -e "\n\n"
         else
             read -p "Enter value for ${KEY} (${DESC}): " VAL
             while [[ -z ${VAL} ]]; do
                 echo -e "value can't be empty"
                 read -p "Enter value for ${KEY} (${DESC}): " VAL
+                echo -e "\n\n"
             done
         fi
         echo "set ${KEY} = ${VAL}"
@@ -72,13 +79,13 @@ while read -r -u9 line; do
 done 9< <(grep SETUP_ENV ${DEFAULTS_FILE})
 
 if [[ ! -e README-Concierge.md ]]; then
-    echo "blanking README"
+    echo -e "blanking README\n\n"
     mv README.md README-Concierge.md
     echo "# New Concierge Project Playbook" > README.md
 fi
 
 # remove tmp files
-echo -e "tidying up..."
+echo -e "tidying up...\n\n"
 [[ -e ${CONFIG_FILE}.swp ]] && rm ${CONFIG_FILE}.swp
 [[ -e ${DEFAULTS_FILE}.swp ]] && rm ${DEFAULTS_FILE}.swp
 
@@ -93,17 +100,23 @@ if [[ -n ${GIT_INITIALISATION} ]]; then
         read -e -p "Do an initial commit (Y/n): " COMMIT
         COMMIT="${COMMIT:-${DO_COMMIT}}"
         COMMIT=$(echo ${COMMIT} | tr '[:upper:]' '[:lower:]')
+        echo -e "\n\n"
     done
     if [[ ${COMMIT} == y ]]; then
-        echo "adding files"
+       echo "adding files"
        git add .
        echo "committing"
        git commit -am "Initial Commit"
+       echo -e "\n\n"
        echo "pushing"
        git push --set-upstream origin master
+       [[ $? -eq 0 ]] && echo -e "Done!\n" || echo -e "Something went wrong! :(\n"
     elif [[ ${COMMIT} == n ]]; then
         exit 0
+        echo -e "Done!\n"
     fi
+else
+    echo -e "Done!\n"
 fi
 
 # verify success
